1a7e66aaa17d3db4662297ae389005aba51ab00a
DevTotoro <totoro.softwaredev@gmail.com>
2025-09-19 01:15:02 +0100

    feat: setup project configuration files

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const prettierConfig = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
+};
+
+export default prettierConfig;
