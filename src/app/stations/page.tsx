80372a1af39add150927d01bc245f6b9c20bb39d
DevTotoro <totoro.softwaredev@gmail.com>
2025-09-20 02:49:13 +0100

    feat: create station list

--- src/app/stations/page.tsx
+++ src/app/stations/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 
+import { stations } from '@/lib/config';
+
 import { PageHeader } from '@/components/page-header';
+import { StationCard } from '@/components/station-card';
 
 const title = 'Stations';
 
@@ -12,6 +15,12 @@ export default function Stations() {
   return (
     <div className="flex flex-col gap-8 py-6 lg:py-8">
       <PageHeader title={title} description="A list of all stations in Derail Valley" />
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {Object.keys(stations).map(stationId => (
+          <StationCard key={stationId} stationId={stationId} />
+        ))}
+      </div>
     </div>
   );
 }
