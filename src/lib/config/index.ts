7c7e82d665ee39105150e0ab32f267e57507c511
DevTotoro <totoro.softwaredev@gmail.com>
2025-09-20 01:22:16 +0100

    chore: update config structure

---
+++ src/lib/config/index.ts
@@ -0,0 +1,2 @@
+export * from '@/lib/config/site-config';
+export * from '@/lib/config/locomotive-config';
--- src/lib/config/locomotive-config.ts
+++ src/lib/config/locomotive-config.ts
@@ -1,29 +1,3 @@
-export const siteConfig = {
-  name: 'Derail Valley Planner',
-  description: 'A route planning tool for Derail Valley',
-  keywords: ['derail valley', 'route planner', 'train simulator'],
-  authors: {
-    devTotoro: { name: 'DevTotoro', url: 'https://github.com/DevTotoro' }
-  },
-  links: {
-    github: 'https://github.com/DevTotoro/derail-valley-planner'
-  },
-  navItems: [
-    {
-      label: 'Deliveries',
-      href: '/deliveries'
-    },
-    {
-      label: 'Locomotives',
-      href: '/locomotives'
-    },
-    {
-      label: 'Stations',
-      href: '/stations'
-    }
-  ]
-};
-
 export const locomotiveTypes = {
   dm: 'Diesel-Mechanical',
   de: 'Diesel-Electric',
