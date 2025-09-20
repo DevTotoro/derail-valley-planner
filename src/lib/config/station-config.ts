ae2f1b1ba40b74aa7fc4518128f7450f74c8017c
DevTotoro <totoro.softwaredev@gmail.com>
2025-09-20 01:42:25 +0100

    feat: create station config

---
+++ src/lib/config/station-config.ts
@@ -0,0 +1,28 @@
+export const stations: Record<
+  string,
+  {
+    name: string;
+    hex: string;
+  }
+> = {
+  CME: { name: 'Coal Mine East', hex: '4a4c44' },
+  CMS: { name: 'Coal Mine South', hex: '353b31' },
+  CP: { name: 'Coal Power Plant', hex: '402e24' },
+  CS: { name: 'City South', hex: '707e86' },
+  CW: { name: 'City West', hex: '7a7872' },
+  FF: { name: 'Food Factory', hex: '53799e' },
+  FM: { name: 'Farm', hex: 'a37a31' },
+  FRC: { name: 'Forest Central', hex: '6a8444' },
+  FRS: { name: 'Forest South', hex: '40693b' },
+  GF: { name: 'Goods Factory', hex: '91596c' },
+  HB: { name: 'Harbor', hex: '5e4c63' },
+  IME: { name: 'Iron Mine East', hex: '824c44' },
+  IMW: { name: 'Iron Mine West', hex: '6f3f30' },
+  MB: { name: 'Military Base', hex: '6f633b' },
+  MF: { name: 'Machine Factory', hex: 'a0643b' },
+  OR: { name: 'Oil Refinery', hex: '6a3a51' },
+  OWC: { name: 'Oil Well Central', hex: '343f44' },
+  OWN: { name: 'Oil Well North', hex: '49422f' },
+  SM: { name: 'Steel Mill', hex: '575c63' },
+  SW: { name: 'Sawmill', hex: '947958' }
+};
