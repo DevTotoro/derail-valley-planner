459adec1feda2ae15a2a5165cb72632a8b43f017
DevTotoro <totoro.softwaredev@gmail.com>
2025-09-20 01:16:39 +0100

    feat: create locomotive list

--- src/app/locomotives/page.tsx
+++ src/app/locomotives/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 
+import { locomotives } from '@/lib/config';
+
 import { PageHeader } from '@/components/page-header';
+import { LocomotiveCard } from '@/components/locomotive-card';
 
 const title = 'Locomotives';
 
@@ -12,6 +15,12 @@ export default function Locomotives() {
   return (
     <div className="flex flex-col gap-8 py-6 lg:py-8">
       <PageHeader title={title} description="A list of all locomotives in Derail Valley" />
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {Object.keys(locomotives).map(locomotiveId => (
+          <LocomotiveCard key={locomotiveId} locomotiveId={locomotiveId} />
+        ))}
+      </div>
     </div>
   );
 }
