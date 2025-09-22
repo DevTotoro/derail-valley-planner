bea3c14b684207280871127fefc83a42127d226a
DevTotoro <totoro.softwaredev@gmail.com>
2025-09-22 01:18:33 +0100

    chore: create const list of locomotives & stations

--- src/lib/schemas/delivery.schema.ts
+++ src/lib/schemas/delivery.schema.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
-import { locomotives, stations } from '@/lib/config';
+import { locomotiveModels, stationIds } from '@/lib/config';
 
 export type Locomotive = z.infer<typeof locomotiveSchema>;
 const locomotiveSchema = z.object({
   id: z.string(),
   type: z.literal('locomotive'),
-  model: z.enum(Object.keys(locomotives)),
-  active: z.boolean()
+  model: z.enum(locomotiveModels).optional(),
+  active: z.boolean().optional()
 });
 
 export type Cargo = z.infer<typeof cargoSchema>;
@@ -15,9 +15,9 @@ const cargoSchema = z.object({
   id: z.string(),
   type: z.literal('cargo'),
   jobId: z.string().optional(),
-  destination: z.enum(Object.keys(stations)),
-  weight: z.number().min(0), // tons
-  length: z.number().min(0) // meters
+  destination: z.enum(stationIds).optional(),
+  weight: z.number().min(0).optional(), // tons
+  length: z.number().min(0).optional() // meters
 });
 
 export type RollingStock = z.infer<typeof rollingStockSchema>;
